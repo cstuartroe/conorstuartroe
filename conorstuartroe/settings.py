c9538d47c64661f5ca9fdf69a4e3d9785107c7cc
root <cstuartroe@haverford.edu>
2018-06-10 02:09:11 +0000

    works on droplet now

--- conorstuartroe/settings.py
+++ conorstuartroe/settings.py
@@ -12,18 +12,14 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+from .settings_secret import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c7kbg2@i29p@2mgrfdwkxj!5&+spw*8&0x-o9u+r=x&=ksyp68'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = ['conorstuartroe.herokuapp.com','127.0.0.1','www.conorstuartroe.com']
 
@@ -119,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/var/www/html/conorstuartroe/static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
