316f86c6f29df1e1637d16d3ff966cdd374786d2
Conor <cstuartroe@haverford.edu>
2018-06-17 17:50:45 -0400

    full versions of structure and index page

--- lauvinko/build_pages.py
+++ lauvinko/build_pages.py
@@ -26,20 +26,30 @@ def django_wrap(html):
 ''' + html + '''
 {% endblock %}'''
 
+def titleize(name):
+    return name.replace('_',' ').title()
+
 class LauvinkoPage:
     def __init__(self,location,name,title,pages):
         self.location = location
-        self.id = join_nums(location)
         self.name = name
         self.title = title
         self.pages = pages
+        
         self.section_heirarchy = [pages[join_nums(self.location[:i])].name for i in range(1,len(self.location))]
+        self.children = []
+        if len(self.location) > 1:
+            self.parent = pages[join_nums(self.location[:-1])]
+            self.parent.add_child(self)
+        else:
+            self.parent = None
+        
         self.filename = self.name + '.xml'
         self.containing_dir = os.path.join(*(['src','pages'] + self.section_heirarchy))
         full_path = os.path.join(self.containing_dir, self.filename)
 
         if os.path.isfile(full_path):
-            with open(full_path,'r') as fh:
+            with open(full_path,'r', encoding="utf-8") as fh:
                 self.xml = fh.read()
         else:
             if not os.path.exists(self.containing_dir):
@@ -47,21 +57,47 @@ class LauvinkoPage:
             open(full_path,'a').close()
             self.xml = ''
 
+    def link(self,leading_text):
+        return '<a href="/lauvinko/%s">%s %s %s</a>' % (self.name,leading_text,join_nums(self.location,'.'),self.title)
+
+    def add_child(self,other):
+        self.children.append(other)
+
     def generate_HTML(self):
         self.html = self.xml
-        self.html = django_wrap('<h1>%s %s</h1>\n' % (join_nums(self.location,'.'),self.title) + self.html)
+
+        if self.parent:
+            go_up = '<p class="go-up">%s</p>' % self.parent.link('Go up to ')
+        else:
+            go_up = '<p class="go-up"><a href="/lauvinko">Go up to index page</a></p>'
+        self.html = go_up + self.html
+        
+        header = '<h1>%s %s</h1>\n' % (join_nums(self.location,'.'),self.title)
+        self.html = header + self.html
+
+        if len(self.children) > 0:
+            go_down = '<div class="row">\n'
+            col_sizes = {1:(12,12),2:(6,6),3:(4,4),4:(6,3),5:(4,4),6:(4,4)}
+            for child in self.children:
+                go_down += '<div class="go-down col-xs-%d col-md-%d"><h3>' % (col_sizes[len(self.children)][0],col_sizes[len(self.children)][1])
+                go_down += child.link('')
+                go_down += '</h3></div>\n'
+            go_down += '</div>\n'
+            self.html += go_down
+        
+        self.html = django_wrap(self.html)
         with open(os.path.join('templates','lauvinko',self.name + '.html'),'w',encoding="utf-8") as fh:
             fh.write(self.html)
 
     def __repr__(self):
-        return 'LauvinkoPage(%s,%s,%s,%s)' % (str(self.location),self.name,self.title,str(self.pages))
+        return 'LauvinkoPage(%s,%s,%s)' % (str(self.location),self.name,self.title)
     
 class MyContentHandler(ContentHandler):
     def __init__(self):
         self.location = []
         self.just_closed = False
         self.pages = {}
-        self.names = {}
+        self.pages_in_order = []
         self.content_ul = ''
 
     def startElementNS(self, name, qname, attributes):
@@ -74,9 +110,14 @@ class MyContentHandler(ContentHandler):
             self.just_closed = False
 
             pagename = attributes[(None,'name')]
-            pagetitle = attributes[(None,'title')]
-            self.pages[join_nums(self.location)] = LauvinkoPage(copy(self.location),pagename,pagetitle,self.pages)
-            self.names[pagename] = join_nums(self.location)
+            try:
+                pagetitle = attributes[(None,'title')]
+            except KeyError:
+                pagetitle = titleize(pagename)
+
+            newpage = LauvinkoPage(copy(self.location),pagename,pagetitle,self.pages)
+            self.pages[join_nums(self.location)] = newpage
+            self.pages_in_order.append(newpage)
             
             self.content_ul += '\t'*len(self.location) + '<li><a href="/lauvinko/%s">%s %s</a></li>\n' % (pagename,join_nums(self.location,'.'),pagetitle)
         else:
@@ -98,7 +139,7 @@ class MyContentHandler(ContentHandler):
         for page in self.pages.values():
             page.generate_HTML()
 
-        with open('src/lauvinko_index.xml','r') as fh:
+        with open('src/lauvinko_index.xml','r',encoding="utf-8") as fh:
             self.index_page = fh.read()
         self.index_page = self.index_page.replace('<contents/>',self.content_ul)
         self.index_page = django_wrap(self.index_page)
--- lauvinko/urls.py
+++ lauvinko/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'(?P<name>[a-z_]+)',views.page),
+    re_path(r'(?P<name>[a-z0-9_]+)',views.page),
     path('', views.index, name='index')
 ]
