43aced9bef2dd51fc996318347ccb6d7d13bae27
Conor Stuart Roe <cstuartroe@haverford.edu>
2021-11-16 05:34:54 +0000

    Center NotFound text

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -5,7 +5,7 @@ export default function NotFound(_props: {}) {
     <>
       <div className="col-2 col-md-3"/>
       <div className="col-8 col-md-6">
-        <div className={'lander-plate'} style={{marginTop: '30vh'}}>
+        <div className={'lander-plate'} style={{marginTop: '30vh', width:'100%'}}>
           <p style={{textAlign: 'center', paddingBottom: 0}}>
             Woah, hold your horses there, partner. This ain't a page.
           </p>
