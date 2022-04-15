b822a18b12db0ccc1dfba0da1b4ecf589a61070f
Conor Stuart Roe <cstuartroe@haverford.edu>
2022-04-15 14:26:48 -0400

    Change photo and employer

--- src/Home.tsx
+++ src/Home.tsx
@@ -8,10 +8,9 @@ export default function Home(_props: {}) {
       <div className="col-2 col-md-3"/>
       <div className="col-8 col-md-6">
         <div id="mainframe">
-          <img className="circle" src="/static/img/conor.png" style={{width: 'min(40vw, 30vh)', height: 'min(40vw, 30vh)'}}/>
+          <img className="circle" src="/static/img/conor.jpg" style={{width: 'min(40vw, 30vh)', height: 'min(40vw, 30vh)'}}/>
           <p style={{textAlign: "center"}}>
-            I write software and make art, often at the same time. I currently work at{' '}
-            <a href="https://www.academia.edu/">Academia.edu</a>.
+            I write software and make art, often at the same time. I currently work at Google.
           </p>
           <SpinningGear/>
         </div>
